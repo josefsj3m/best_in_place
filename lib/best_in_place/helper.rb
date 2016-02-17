274fe1639988aa553546ee16e9ef3db1232ecc6f
Jose Fernando <josefsmvm@gmail.com>
2016-02-17 08:26:43 -0200

    Added masked input support.

--- lib/best_in_place/helper.rb
+++ lib/best_in_place/helper.rb
@@ -46,7 +46,7 @@ module BestInPlace
       options[:data]['bip-cancel-button-class'] = opts[:cancel_button_class].presence
       options[:data]['bip-original-content'] = html_escape(opts[:value] || value).presence
 
-      options[:data]['data-mask'] = opts[:mask].presence
+      options[:data]['mask'] = opts[:mask].presence
 
       options[:data]['bip-skip-blur'] = opts.has_key?(:skip_blur) ? opts[:skip_blur].presence : BestInPlace.skip_blur
 
